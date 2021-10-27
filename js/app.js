7ddc33d3291483494c4ecf50a8149dc6d2f1ab85
Marco Messina <voldrak94@gmail.com>
2021-10-27 21:54:05 +0200

    Finito, ma non del tutto

--- js/app.js
+++ js/app.js
@@ -8,3 +8,41 @@ lighMode.addEventListener("click", () => {
     const dayMode = document.querySelector(".hobby");
     dayMode.classList.toggle("illumi");
 });
+
+//Conta i paragrafi
+window.addEventListener( "click", () => {
+let paragrafi = document.getElementsByTagName("p");
+console.log(paragrafi)
+},
+{once: true}
+)
+
+//Test
+let uno = document.getElementById("uno");
+let due = document.getElementById("due");
+let tre = document.getElementById("tre");
+let quattro = document.getElementById("quattro");
+let cinque = document.getElementById("cinque");
+
+uno.onclick = function(){
+    uno.style.backgroundColor = "red";
+    tre.style.backgroundColor = "red";
+}
+
+due.onclick = function(){
+    due.style.backgroundColor = "red";
+    quattro.style.backgroundColor = "red";
+}
+
+tre.onclick = function(){
+    tre.style.backgroundColor = "red";
+    cinque.style.backgroundColor = "red";
+}
+quattro.onclick = function(){
+    quattro.style.backgroundColor = "red";
+    uno.style.backgroundColor = "red";
+}
+cinque.onclick = function(){
+    cinque.style.backgroundColor = "red";
+    due.style.backgroundColor = "red";
+}
