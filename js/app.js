0ebfb92a97922ef16c1cd1ad1f6ae4d4046ea044
Marco Messina <voldrak94@gmail.com>
2021-10-28 09:45:30 +0200

    completato ma...

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 //Dark Mode
 const darkMode = document.querySelector("#enableDarkMode");
-darkMode.addEventListener("click", () => { document.body.classList.toggle("nightMode");});
+darkMode.addEventListener("click", () => 
+{ document.body.classList.toggle("nightMode");});
 
 //Illuminazione Hobby
 const lighMode = document.querySelector("#luce");
@@ -18,7 +19,7 @@ console.log(paragrafi)
 )
 
 //Test
-let uno = document.getElementById("uno");
+/*let uno = document.getElementById("uno");
 let due = document.getElementById("due");
 let tre = document.getElementById("tre");
 let quattro = document.getElementById("quattro");
@@ -45,4 +46,45 @@ quattro.onclick = function(){
 cinque.onclick = function(){
     cinque.style.backgroundColor = "red";
     due.style.backgroundColor = "red";
-}
+}*/
+
+
+const puno = document.querySelector("#uno");
+    puno.addEventListener("click", () => {
+    const ppuno = document.querySelector("#tre");
+    ppuno.classList.toggle("uno");
+    const pppuno = document.querySelector("#uno");
+    pppuno.classList.toggle("uno");
+});
+
+const pdue = document.querySelector("#due");
+    pdue.addEventListener("click", () => {
+    const ppdue = document.querySelector("#due");
+    ppdue.classList.toggle("due");
+    const pppuno = document.querySelector("#quattro");
+    pppuno.classList.toggle("due");
+});
+
+const ptre = document.querySelector("#tre");
+    ptre.addEventListener("click", () => {
+    const pptre = document.querySelector("#tre");
+    pptre.classList.toggle("tre");
+    const ppptre = document.querySelector("#cinque");
+    ppptre.classList.toggle("tre");
+});
+
+const pquattro = document.querySelector("#quattro");
+    pquattro.addEventListener("click", () => {
+    const ppquattro = document.querySelector("#quattro");
+    ppquattro.classList.toggle("quattro");
+    const pppquattro = document.querySelector("#uno");
+    pppquattro.classList.toggle("quattro");
+});
+
+const pcinque = document.querySelector("#cinque");
+    pcinque.addEventListener("click", () => {
+    const ppcinque = document.querySelector("#cinque");
+    ppcinque.classList.toggle("cinque");
+    const pppcinque = document.querySelector("#due");
+    pppcinque.classList.toggle("cinque");
+});
